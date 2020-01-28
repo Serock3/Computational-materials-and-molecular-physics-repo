227ee8feb89f107eceb8193649ca2f002564187e
Sebastian Holmin <sebastian.holmin@hotmail.com>
2020-01-28 23:24:28 +0100

    .

---
+++ main.py
@@ -0,0 +1,13 @@
+#%%
+import numpy as np
+
+alpha = (0.298073,1.242567,5.782948,38.474970)
+
+C = [1,1,1,1]
+
+def normalize(asd):
+    return [n/np.sqrt(np.dot(asd,asd)) for n in asd]
+
+C = normalize(C)
+
+# %%

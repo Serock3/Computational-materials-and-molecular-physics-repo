ad3e1f81e16a584ae1a905ef810105add2a65155
Sebastian Holmin <sebastian.holmin@hotmail.com>
2020-01-31 14:15:58 +0100

    Co-authored-by: ErikAndersson1997 <erianb@student.chalmers.se>

---
+++ task1.py
@@ -0,0 +1,99 @@
+# %%
+import matplotlib.pylab as plt
+import numpy as np
+import seaborn as sns
+import scipy
+sns.set('talk')
+
+# Given alpha values
+a = [0.297104, 1.236745, 5.749982, 38.216677]
+
+# Init of matrices from Thijssen 4.3.2
+# h matrix
+h = np.zeros((4, 4))
+for p in range(4):
+    for q in range(4):
+        h[p, q] = 4*np.pi/(a[p]+a[q])*(3/4*a[q] *
+                                       (1-a[q]/(a[p]+a[q]))*np.sqrt(np.pi/(a[p]+a[q]))-1)
+
+# Q matrix (given by eq 4.17 in Thijssen)
+Q = np.zeros((4, 4, 4, 4))
+for p in range(4):
+    for q in range(4):
+        for r in range(4):
+            for s in range(4):
+                Q[p, r, q, s] = 2*np.pi**(5/2)/((a[p]+a[q])
+                                                * (a[r]+a[s])*np.sqrt(a[p]+a[q]+a[r]+a[s]))
+
+# S matrix
+S = np.zeros((4, 4))
+for p in range(4):
+    for q in range(4):
+        S[p, q] = (np.pi/(a[p]+a[q]))**(3/2)
+
+# Inital values
+C = [1, 1, 1, 1]
+
+
+def normalize(C):
+    return C/np.sqrt(np.matmul(C, np.matmul(S, C)))
+
+
+F = np.zeros((4, 4))
+
+
+def getEg(C, h, Q):
+    def con(Q, C):
+        return np.tensordot(Q, C, axes=([0], [0]))
+    return 2*np.matmul(C, np.matmul(h, C))+con(con(con(con(Q, C), C), C), C)
+
+
+MaxIters = 15
+E = np.zeros(MaxIters)
+
+for i in range(MaxIters):
+    C = normalize(C)
+    E[i] = getEg(C, h, Q)
+    print(E[i])
+
+    if np.abs(E[i]-E[i-1]) < 1e-5/27.21:
+        break
+    # Set F(C)
+    for p in range(4):
+        for q in range(4):
+            F[p, q] = h[p, q]+np.matmul(C, np.matmul(Q[p, :, q, :], C))
+
+    # Sovle eigenvalue problem
+    (eps, w) = scipy.linalg.eig(F, S)
+
+    if any(eps.imag != 0):
+        raise Exception('complex eig')
+
+    #w = w[:,E.imag==0].real
+    #E = E[E.imag==0].real
+
+    # Get best C
+    C = w[:, np.argmin(eps.real)]  # np.argmin(E)]
+    #E = E[np.argmin(E.real)].real
+
+C = normalize(C)
+print("Eg = ", getEg(C, h, Q))
+print("C= ", C)
+
+# Print the radial PDF
+Rmax = 5  # atomic
+N = 1000
+h = Rmax/N
+r = np.linspace(h, Rmax, N)
+
+plt.figure(figsize=(8, 6))
+plt.xlabel(r"Distance [$a_0$]", fontsize=18)
+plt.ylabel("Radial PDF", fontsize=18)
+plt.plot(r, 4*np.pi*r**2 *
+         np.sum([C[i]*np.exp(-a[i]*r**2) for i in range(4)], axis=0)**2)
+plt.savefig('task1.pdf')
+
+
+
+
+# %%
---
+++ task2.py
@@ -0,0 +1,177 @@
+# %%
+import matplotlib.pylab as plt
+import numpy as np
+import seaborn as sns
+import scipy
+sns.set('talk')
+
+def getVH(ns,N,Rmax):
+    h = Rmax/N
+    A = np.diag(-2*np.ones(N),0)+np.diag(np.ones(N-1),1)+np.diag(np.ones(N-1),-1)
+    r=np.linspace(h,Rmax,N)
+    tmp = -4*np.pi*h**2*r*ns
+    tmp[-1] -= 1
+    U = np.linalg.solve(A,tmp)
+    return U/r
+
+def getpsi(V,N,Rmax,Z):
+    h = Rmax/N
+    r = np.linspace(h,Rmax,N)
+    c = 1/h**2-Z/r+V
+    A = np.diag(c,0)+np.diag(-np.ones(N-1)/(2*h**2),1)+np.diag(-np.ones(N-1)/(2*h**2),-1)
+    
+    (E, w) = np.linalg.eig(A)
+    eps = np.min(E).real
+    f = w[:,np.argmin(E)]
+    f = f/np.sqrt(np.trapz(f**2,r))*np.sign(f[0])
+    psi = 1/np.sqrt(4*np.pi)*f/r
+    return (psi,eps)
+
+def getE(eps,VH,Vxc,epsxc,ns,Z):
+    return Z*eps-Z*4*np.pi*np.trapz((VH*ns/2+Vxc*ns-epsxc*ns)*r**2,r)
+
+
+#%% task 2
+
+Rmax = 10 # atomic
+Z=1
+N=1000
+h = Rmax/N
+r = np.linspace(h,Rmax,N)
+ns=1/np.pi*Z**3*np.exp(-2*Z*r)
+VH=getVH(ns,N,Rmax)
+VHanalytic = 1/r-(1+1/r)*np.exp(-2*r)
+
+plt.figure(figsize=(8, 6))
+plt.plot(r,VH, label = r'Hartree method')
+plt.plot(r,VHanalytic, '--' ,label = r'Analytical')
+plt.legend()
+
+plt.xlabel(r"Distance [$a_0$]", fontsize=18)
+plt.ylabel("Potential [Ha]", fontsize=18)
+plt.savefig('task2.pdf')
+#%% task 3
+
+Rmax = 6 # atomic
+Z=1
+N=1000
+h = Rmax/N
+r = np.linspace(h,Rmax,N)
+
+(psi,eps) = getpsi(0,N,Rmax,Z)
+print("eps = ", eps)
+ns= np.abs(psi)**2
+print("E = ",getE(eps,0,0,0,ns,Z))
+
+
+plt.figure(figsize=(8, 6))
+plt.xlabel(r"Distance [$a_0$]", fontsize=18)
+plt.ylabel("Radial PDF", fontsize=18)
+plt.plot(r,4*np.pi*r**2*ns)
+plt.savefig('task3.pdf')
+#%% Task 4
+Z= 2
+N=800
+Rmax = 6 # atomic
+h = Rmax/N
+r = np.linspace(h,Rmax,N)
+ns=1/np.pi*Z**3*np.exp(-2*Z*r)
+
+MaxIters = 15
+E = np.zeros(MaxIters)
+
+for i in range(MaxIters):
+    VH = getVH(ns,N,Rmax)
+    (psi,eps) = getpsi(VH,N,Rmax,Z)
+    ns = np.abs(psi)**2
+    E[i] =getE(eps,VH,0,0,ns,Z)
+    if np.abs(E[i]-E[i-1])<1e-5/27.21:
+        break
+print("eps = ", eps)
+print("E = ", E[i])
+plt.figure(figsize=(8, 6))
+plt.xlabel(r"Distance [$a_0$]", fontsize=18)
+plt.ylabel("Radial PDF", fontsize=18)
+plt.plot(r,4*np.pi*r**2*ns)
+plt.savefig('task4.pdf')
+#%% Task 5
+Z= 2
+N=1000
+Rmax = 6 # atomic
+h = Rmax/N
+r = np.linspace(h,Rmax,N)
+
+ns=1/np.pi*Z**3*np.exp(-2*Z*r)
+
+epsx= - 3/4*(3*ns/np.pi)**(1/3)
+ndepsx= 1/3*epsx
+Vx = epsx + ndepsx
+
+MaxIters = 30
+E = np.zeros(MaxIters)
+
+for i in range(MaxIters):
+    VH = 2*getVH(ns,N,Rmax)
+    (psi,eps) = getpsi(VH+Vx,N,Rmax,Z)
+    ns = np.abs(psi)**2
+    E[i] =getE(eps,VH,Vx,epsx,ns,Z)
+    if np.abs(E[i]-E[i-1])<1e-5/27.21:
+        break
+print("E = ", E[i])
+plt.figure()
+plt.plot(E[:i])
+plt.figure(figsize=(8, 6))
+plt.xlabel(r"Distance [$a_0$]", fontsize=18)
+plt.ylabel("Radial PDF", fontsize=18)
+plt.plot(r,4*np.pi*r**2*ns)
+plt.savefig('task5.pdf')
+# %% Task 6
+Z= 2
+N=500
+Rmax = 7 # atomic
+h = Rmax/N
+r = np.linspace(h,Rmax,N)
+
+ns=1/np.pi*Z**3*np.exp(-2*Z*r)
+
+A = 0.0311
+B = -0.048
+C = 0.002
+D = -0.0116
+gamma = -0.1423
+beta1 = 1.0529
+beta2 = 0.3334
+
+rs=(3/(4*np.pi*ns))**(1/3)
+
+epsx=- 3/4*(3*ns/np.pi)**(1/3)
+ndepsx= 1/3*epsx
+ndepsc=(rs>=1)*ns*(-(gamma*(beta2+beta1/(2*np.sqrt(rs)))*4*np.pi*rs**4/9)/(1+beta1*np.sqrt(rs)+beta2*rs)**2)
+ndepsc=ndepsc+(rs<1)*ns*(A/rs+C+C*np.log(rs)+D)*4*np.pi*rs**4/9
+
+epsc=(rs>=1)*gamma/(1+beta1*np.sqrt(rs)+beta2*rs)
+epsc=epsc+(rs<1)*(A*np.log(rs)+B+C*rs*np.log(rs)+D*rs)
+epsxc = epsx + epsc
+
+Vx = epsx + ndepsx
+Vc = epsc + ndepsc
+Vxc = Vx + Vc 
+
+
+MaxIters = 30
+E = np.zeros(MaxIters)
+
+for i in range(MaxIters):
+    VH = 2*getVH(ns,N,Rmax)
+    (psi,eps) = getpsi(VH+Vxc,N,Rmax,Z)
+    ns = np.abs(psi)**2
+    E[i] =getE(eps,VH,Vxc,epsxc,ns,Z)
+    if np.abs(E[i]-E[i-1])<1e-5/27.21:
+        break
+print("E = ", E[i])
+plt.plot(r,4*np.pi*r**2*ns)
+plt.figure()
+plt.plot(E[:i])
+
+
+# %%

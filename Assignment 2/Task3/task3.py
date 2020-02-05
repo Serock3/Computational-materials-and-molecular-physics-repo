46559a08d2b5a241923e23cfc23dcff5c00f7afe
Sebastian Holmin <holmins@hebbe2.int.private>
2020-02-05 15:30:09 +0100

    Co-authored-by: ErikAndersson1997 <erianb@student.chalmers.se>

--- Assignment 2/Task3/task3.py
+++ Assignment 2/Task3/task3.py
@@ -1,14 +1,10 @@
-
 #%%
-
 from ase.db import connect
 from ase.ga.data import DataConnection
 from ase.optimize import GPMin
 from ase import Atoms, Atom
 from gpaw import GPAW, FermiDirac
-from ase.io import read
-
-
+from ase.io import read, write
 
 a=Atoms(read("guess.xyz", format='xyz'))
 
@@ -23,6 +19,14 @@ calc = GPAW(nbands=10,
 
 a.set_calculator(calc)
 #print('Relaxing starting candidate {0}'.format(a.info['confid']))
+print("Relax start")
 dyn = GPMin(a, trajectory='relax_ref.traj', logfile='relax_ref.log')
 dyn.run(fmax=0.02, steps=2)
+print("Relax done\n")
+print("-a.get_potential_energy() ",-a.get_potential_energy())
+#a.info['key_value_pairs']['raw_score'] = -a.get_potential_energy()
+
+# da.add_relaxed_step(a)
+write('post.xyz', a)
 # %%
+1+1
